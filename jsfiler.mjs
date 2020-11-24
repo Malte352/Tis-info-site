360eb329f9745e63b5705b86c3f814c81d5ad347
Malte352 <71072454+Malte352@users.noreply.github.com>
2020-11-24 11:50:38 +0100

    Add files via upload

--- jsfiler.mjs
+++ jsfiler.mjs
@@ -1,1 +1,29 @@
-js-filer
+function viewParent()
+{
+    // Sparar element-typen i variabeln "parentTag"
+    var parentTag = this.parentNode.tagName;
+     
+    // Sparar elementets id i variabeln "parentId"
+    var parentId = this.parentNode.id;
+     
+    // Skriver ut informationen som samlats in ovan
+    alert("Föräldern till denna nod är av element-typen: " + parentTag + "\nOch har id: " + parentId);
+}
+ 
+ 
+function init()
+{
+ 
+    // Läggen event-handlers på våra knappar
+ 
+    var button1 = document.getElementById("button1")
+    button1.onclick = viewParent;
+     
+    var button2 = document.getElementById("button2")
+    button2.onclick = viewParent;
+     
+    var button3 = document.getElementById("button3")
+    button3.onclick = viewParent;   
+}
+ 
+window.onload = init;
